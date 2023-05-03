e2a2aa2032e97bafdb92611b6f4e2207dd0db98c
kechkarayoub <kechkarayoub@gmail.com>
2023-05-04 00:33:14 +0100

    first commit

--- client/script.js
+++ client/script.js
@@ -64,7 +64,7 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000/", {
+  const response = await fetch(process.env.BACKEND_URL, {
     method: "POST",
     headers: {
       'Content-Type': 'application/json',
